e5f27fc43a33151ddf45b5c050832831adc1d147
Trev Nowak <teeerevor@gmail.com>
2013-12-20 20:45:07 +1100

    update current year

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-ENV['current_year'] = '2012'
+ENV['current_year'] = '2013'
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
