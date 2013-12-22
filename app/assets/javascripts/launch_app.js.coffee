88896e9dbdb6c0f34533014b470867e1d9ab4c10
Trev Nowak <teeerevor@gmail.com>
2013-12-22 22:14:17 +1100

    No loads short list from url

--- app/assets/javascripts/launch_app.js.coffee
+++ app/assets/javascripts/launch_app.js.coffee
@@ -37,18 +37,21 @@ window.App = {
 
   load_backbone: ->
     #year and email are set in the app layout
-    window.songsList = new Hotmess.Collections.Songs(gon.songs,{year: urlYear})
+    window.songsList = new Hotmess.Collections.Songs(gon.songs)
     window.songListView = new Hotmess.Views.SongsListView({collection: songsList})
     $('#song_list').append(songListView.render().el)
 
     window.saveLoadView = new Hotmess.Views.SaveLoadView({})
     $('#header').append(saveLoadView.render().el)
+    saveLoadView.setEmailFromUrlLoad(urlEmail) if urlEmail
 
     window.hottestPlayer = new Hotmess.Views.PlayerView({})
     $('#header').append(hottestPlayer.render().el)
 
     #load these last - will break url email to saveload push otherwise
-    window.shortList = new Hotmess.Collections.ShortList([],{year: urlYear, email: urlEmail})
+    sl = []
+    sl = gon.short_list if gon.short_list
+    window.shortList = new Hotmess.Collections.ShortList(sl, {year: urlYear, email: urlEmail})
     window.shortListView = new Hotmess.Views.ShortListView({collection: shortList})
     $('#short_list').append(shortListView.render().el)
 
--- app/controllers/short_lists_controller.rb
+++ app/controllers/short_lists_controller.rb
@@ -2,11 +2,23 @@ class ShortListsController < ApplicationController
   def show
     @email = params[:email]
     @year = params[:year] || ENV['current_year']
+
     @short_list = ShortList.find_by_email_and_year(@email, @year)
     @short_list_songs = @short_list.short_listed_songs.all( :include => {:song => :artist}, :order => 'short_listed_songs.position')
     @songs = @short_list_songs.map{|sls| sls.song}
+    gon.rabl "app/views/songs/index.json.rabl", as: 'songs'
+
+    setup_for_html if request.format == :html
 
     render 'songs/index'
+
+  rescue
+    error_msg = "Short list for that email does not exist"
+    respond_to do |format|
+      format.html { redirect_to root_path, :flash => { :error => "Short list for that email does not exist" }}
+      format.json { render status: 404, json: @controller.to_json }
+    end
+    
   end
 
   def update
@@ -18,7 +30,18 @@ class ShortListsController < ApplicationController
         @short_list = ShortList.create(email: email, year: year)
       end
       @short_list.short_list_songs(songs_list)
+      render status: 200, json: @controller.to_json
     end
+  end
+
+  def setup_for_html
+    gon.rabl "app/views/songs/index.json.rabl", as: 'short_list'
+    load_song_list
+  end
+
+  def load_song_list
+    @songs = Song.find_for_year(@year)
+    gon.rabl "app/views/songs/index.json.rabl", as: 'songs'
   end
 end
 
