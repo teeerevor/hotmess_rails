83d23ef40412525d700597bf29a635bdf9ea2ef6
Trev Wolf <teeerevor@gmail.com>
2016-01-24 20:00:41 +1100

    update vendors and remove backbone

--- vendor/assets/javascripts/vendor.js
+++ vendor/assets/javascripts/vendor.js
@@ -1,11 +1,8 @@
 /*
 *= require underscore-min
-*= require backbone-min
-*= require backbone.dualstorage
 *= require handlebars-1.0.0.beta6
 *= require jquery.fitvids
 *= require animatescroll.min
-*= require swfobject
 *= require ui-progress-bar
 */
 
