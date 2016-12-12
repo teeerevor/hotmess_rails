c6bc2c3cb9d590437a24db36f334c85fc05a5e31
Trev <teeerevor@gmail.com>
2016-12-12 21:04:27 +1100

    hmm why you no work

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,11 +5,11 @@ class SongsController < ApplicationController
     @year = ENV['current_year']
     #@albums = Song.where(year: @year).where.not(album_img_url: nil).map(&:album_img_url).uniq.shuffle.sample 30
     @albums = []
-    @songs = Song.includes(:artist).where(year: @year).order(:name).limit 100
+    @songs = Song.includes(:artist).where(year: 2016).order(:name).limit 100
     gon.rabl "app/views/songs/index.json.rabl", as: 'initial_songs'
-    @songs = Song.includes(:artist).where(year: @year).order(:name)
+    @songs = Song.includes(:artist).where(year: 2016).order(:name)
     gon.rabl "app/views/songs/index.json.rabl", as: 'songs'
-    @songs = Song.includes(:artist).where(year: @year).order('artists.name')
+    @songs = Song.includes(:artist).where(year: 2016).order('artists.name')
     gon.rabl "app/views/songs/index.json.rabl", as: 'songs_by_artists'
   end
 end
